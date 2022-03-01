98f40a0f9608985372a5484a0cf8741283368665
Patel-Dhruvil <dhruvil.patel@einfochips.com>
2022-03-01 18:41:51 +0530

    Add project files.

---
+++ GuniKitchen/Models/MyIdentityRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace GuniKitchen.Models
+{
+    public class MyIdentityRole 
+        : IdentityRole<Guid>
+    {
+    }     
+}
---
+++ GuniKitchen/Models/MyidentityUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuniKitchen.Models
+{
+    public class MyIdentityUser
+         : IdentityUser<Guid>
+    {
+
+        [Display(Name = "Display Name")]
+        [Required(ErrorMessage = "{0} cannot be empty.")]
+        [MinLength(2, ErrorMessage = "{0} should have at least {1} characters.")]
+        [MaxLength(60, ErrorMessage = "{0} cannot have more than {1} characters.")]
+        public string DisplayName { get; set; }
+
+
+        [Display(Name = "Date of Birth")]
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "smalldatetime")]
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
